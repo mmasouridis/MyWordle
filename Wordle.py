31c5856b74ea424eb4a7046c71fddd731a58c8fc
m.masouridis <m.masouridis@adveos.com>
2022-06-15 00:39:30 +0300

    Removed unecessary gaps from database. Game is functional but there is a bug when there are duplicate letters

--- Wordle.py
+++ Wordle.py
@@ -12,34 +12,44 @@ def New_Game():
 def import_DB():
     words=[]
     db=open("DB.txt","r")
-    for word in db:
-        words.append(db.read())
-    print(words)
+    words=db.readlines()
     return words
 
 
-def Main():
+answer='y'
+words=import_DB()
+#print(word)
+while answer=='y':
     tries=6
-    words=import_DB()
-    word=words[random.randint(0,len(words))]
-    print(word)
-    New_Game()
+    word=words[random.randint(0,len(words))].strip('\n')
+    print("Tries remaining: ",tries)
     guess=input("Your guess:")
     while True:
-        while len(guess)!=5: 
-            print("Input word must have 5 letters.")
-            guess=input("Your guess:")
+        while len(guess)!=5: #or (guess not in words): 
+          if len(guess)!=5: print("Input word must have 5 letters.")
+          else:print("Your guess is not in the word list.")
+          guess=input("Your guess:")
         for letter in guess:
-            if letter not in word : print(colorama.Fore.RED + letter)
-            else: print(colorama.Fore.GREEN + letter)
+          if letter not in word : print(colorama.Fore.RED + letter,end='')
+          else: 
+            if guess.index(letter)!=word.index(letter):
+                print(colorama.Fore.YELLOW + letter,end='')
+            else:
+                print(colorama.Fore.GREEN + letter,end='')
+        print(colorama.Style.RESET_ALL)
         if guess==word:
-            print("YOU WON :D")
-            break
+          print("YOU WON :D")
+          break
         else:
-            tries-=1
-            if tries==0:
-                print("You Lost :( , The correct word was %s",word)
-                break
+          tries-=1
+          print("Tries remaining: ",tries)
+          if tries==0:
+            print("You Lost :( , The correct word was ",word)
+            break
+        guess=input("Your guess:")
+    answer=input("Do you want to play again[y/n]: ")
+    while answer!='y' and answer!='n':answer=input("Do you want to play again[y/n]: ")
+    
+    
 
-import_DB()
-Main()
+
